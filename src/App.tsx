ac694778edf63ee3c25a8ebd9ef1bbd3cca11db3
Toby Hasler-Winter <toby.hw@live.co.uk>
2024-08-08 03:29:16 +0100

    ADD Data table display with Approved Status true/false

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import "./App.css";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-quartz.css";
+import reversalRequestData from "./assets/reversalRequestData.json";
+import { ColDef } from "ag-grid-community";
+import { shouldApproveRefundRequest } from "./shouldApproveRefundRequest";
+import { RequestType } from "./getRequestTimeLimit";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface RequestData {
+  name: string;
+  timezone: string;
+  signUpDate: string;
+  requestSource: string;
+  investmentDate: string;
+  investmentTime: string;
+  refundRequestDate: string;
+  refundRequestTime: string;
 }
 
-export default App
+function App() {
+  const [rowData] = useState<RequestData[]>(reversalRequestData);
+
+  const [colDefs] = useState<ColDef<RequestData>[]>([
+    { field: "name" },
+    {
+      headerName: "Approved",
+      valueGetter: (p) =>
+        p.data &&
+        shouldApproveRefundRequest(
+          p.data?.timezone,
+          p.data?.signUpDate,
+          p.data?.requestSource as RequestType,
+          p.data?.investmentDate,
+          p.data?.investmentTime,
+          p.data?.refundRequestDate,
+          p.data?.refundRequestTime
+        ),
+    },
+    { field: "timezone" },
+    { field: "signUpDate" },
+    { field: "requestSource" },
+    { field: "investmentDate" },
+    { field: "investmentTime" },
+    { field: "refundRequestDate" },
+    { field: "refundRequestTime" },
+  ]);
+
+  const defaultColDef = useMemo(() => {
+    return {
+      width: 150,
+      cellStyle: { fontWeight: "bold" },
+    };
+  }, []);
+  return (
+    <div className="ag-theme-quartz" style={{ height: 600, width: 1360 }}>
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+      />
+    </div>
+  );
+}
+
+export default App;
